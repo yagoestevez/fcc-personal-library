6ec6e5b489c2df9f1579b823a9908a4473b864d8
Yago Estevez <inbox@yagoestevez.com>
2018-07-20 13:47:40 +0200

    Begin with boilerplate code for server.js

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ To test the app, just run ```npm test``` (after ```npm i```, of course).
 
 1. ADD YOUR MongoDB connection string to .env without quotes as db ```example: DB=mongodb://admin:pass@1234.mlab.com:1234/fccpersonallib```
 2. SET NODE_ENV to `test` without quotes
-3. You need to create all routes within `routes/api.js`
-4. You will add any security features to `server.js`
+3. You need to create all routes within ```routes/api.js```
+4. You will add any security features to ```server.js```
 5. You will create all of the functional tests in ```tests/2_functional-tests.js```
 
 ---
--- server.js
+++ server.js
@@ -1,56 +1,57 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var cors        = require('cors');
+'use strict';
+const express     = require( 'express' );
+const cors        = require( 'cors' );
+const helmet      = require( 'helmet' );
+const dotEnv      = require( 'dotenv' ).config( );
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes   = require( './routes/api.js' );
+const fccTesting  = require( './routes/fcctesting.js' );
+const runner      = require( './test-runner' );
 
-var app = express();
+const app  = express( );
+const PORT = process.env.PORT || 3000;
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use( '/static', express.static( 'public' ) );
+app.use( cors( { origin: '*' } ) ); //For FCC testing purposes only
+app.use( express.urlencoded( { extended: true } ) );
+app.use( helmet( {
+  noCache       : true,
+  hidePoweredBy : { setTo: 'PHP 4.2.0' },
+  xssFilter     : true,
+} ) );
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+// Index page (static HTML).
+app.get( '/', ( req,res ) => {
+  res.sendFile( process.cwd( ) + '/views/index.html' );
+} );
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+fccTesting( app ); // For FCC testing purposes.
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
+apiRoutes( app );
     
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+// 404 Not Found Middleware.
+app.use( ( req,res,next ) => {
+  res.status( 404 )
+    .type( 'text' )
+    .send( 'Not Found' );
+} );
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
+// Start the server and testing suite.
+app.listen( PORT, ( ) => {
+  console.log( "Listening on port " + PORT );
+  if ( process.env.NODE_ENV === 'test' ) {
+    console.log( 'Running Tests...' );
+    setTimeout( ( ) => {
       try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        runner.run( );
+      } catch( error ) {
+        console.log( 'Tests are not valid:' );
+        console.log( error );
       }
-    }, 1500);
+    }, 3500 );
   }
-});
+} );
 
-module.exports = app; //for unit/functional testing
+module.exports = app; // For testing.
